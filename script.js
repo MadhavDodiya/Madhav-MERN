929181a849a6257eda8ab954396c6365e10cfc1d
i32Excelsior <i3.2.excelsior@gmail.com>
2025-11-25 11:44:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const carousel = new bootstrap.Carousel(document.getElementById('cardCarousel'), {
+    interval: 3000,
+    ride: 'carousel'
+  });
+});
+
+document.querySelectorAll('.circle').forEach(circle => {
+  const percent = circle.getAttribute('data-percent');
+  const radius = 45;
+  const circumference = 2 * Math.PI * radius;
+  const progress = circle.querySelector('.progress');
+
+  progress.style.strokeDasharray = circumference;
+  progress.style.strokeDashoffset = circumference - (percent / 100) * circumference;
+});
+
+var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
